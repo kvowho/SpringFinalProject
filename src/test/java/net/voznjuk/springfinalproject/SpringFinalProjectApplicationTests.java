ec994eb27e1c1fc178d1923576ab2adb70ea91e7
Kostiantyn Vozniuk <kvoznjuk@gmail.com>
2021-09-22 01:55:51 +0300

    First

---
+++ src/test/java/net/voznjuk/springfinalproject/SpringFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package net.voznjuk.springfinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
